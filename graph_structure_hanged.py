60ff59a4b429c8223f96f2af9696bd63e882b6e2
Nicolas Beltran Castellanos <nicolas.belt.cast@gmail.com>
2022-10-09 23:22:02 -0500

    First upload to repository with initial files

---
+++ graph_structure_hanged.py
@@ -0,0 +1,102 @@
+import random
+
+def random_word():
+    words = open('words.txt', mode ="r",encoding='utf-8')
+    words = words.read()
+    words_list= words.split('\n')
+    words = random.randint(0, len(words_list)) 
+    choosed_word = words_list[words]
+    return choosed_word
+
+def hang_graph():
+    print(HANGED[0])
+    print((len(random_word())) * '_ ')
+    print(random_word())
+
+
+
+
+
+
+
+
+
+
+
+menu="""
+_________________________________________
+          WELCOME TO HANG GAME
+
+  You have six attempts to win this game 
+
+The program choose a word in ramdom order
+and your job is guess what word is or the
+little person on the hang DIE!!!
+
+               LETS PLAY
+_________________________________________
+
+Select the difficult:
+
+ 1) Easy
+ 2) Medium
+ 3) Hard
+
+R//:  """
+
+difficult = int(input(menu))
+
+
+
+HANGED = ['''
+   +---+
+   |   |
+       |
+       |
+       |
+       |
+=============''', '''
+      +---+
+      |   |
+      O   |
+          |
+          |
+          |
+    =========''', '''
+      +---+
+      |   |
+      O   |
+      |   |
+          |
+          |
+    =========''', '''
+      +---+
+      |   |
+      O   |
+     /|   |
+          |
+          |
+    =========''', '''
+      +---+
+      |   |
+      O   |
+     /|\  |
+          |
+          |
+    =========''', '''
+      +---+
+      |   |
+      O   |
+     /|\  |
+     /    |
+          |
+    =========''', '''
+      +---+
+      |   |
+      O   |
+     /|\  |
+     / \  |
+          |
+    ========''']
+
+
---
+++ hanged.py
@@ -0,0 +1,25 @@
+
+import random
+import graph_structure_hanged as graph
+
+
+def run():
+    print(graph.menu)
+    
+    if graph.difficult == 1:
+        print('you select easy mode, good luck')
+        graph.random_word()
+        graph.hang_graph()
+
+
+ 
+   
+    
+
+
+    
+      
+
+  
+if __name__ == '__main__':
+    run()
